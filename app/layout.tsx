226010b647ab0a3f51373bf96fa0f31c98d35235
akshay-shekokar <akshay.shekokar@gmail.com>
2023-05-29 16:56:00 +0530

    Added google ad sense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,22 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className="h-full w-full bg-slate-900 text-slate-200">{children}</body>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2133814916385175"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
+      <body className="h-full w-full bg-slate-900 text-slate-200">
+        {children}
+      </body>
     </html>
-  )
+  );
 }
